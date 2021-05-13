09abd33e89c5c713947cd052694704e6ebd0d5b0
Zachary Deschaux <zachary.deschaux@gmail.com>
2021-05-13 16:30:03 -0400

    Add PipeFitter to integrate clients

--- stream-fitter/src/lib.rs
+++ stream-fitter/src/lib.rs
@@ -1,6 +1,7 @@
 //! Rusty library for linking and interfacing with chat streams.
 pub mod clients;
 pub mod errors;
+pub mod pipe_fitter;
 
 /// Lifted error type used throughout this crate.
 pub type Error = errors::FitterError;
