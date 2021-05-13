a5160d08f76fc6f1beaa9cde23762ae3522fb8d8
Zachary Deschaux <zachary.deschaux@gmail.com>
2021-05-13 17:29:01 -0400

    Add stream-fitter CLI

--- stream-fitter-cli/src/main.rs
+++ stream-fitter-cli/src/main.rs
@@ -1,3 +1,52 @@
+use std::{
+    fs::File,
+    panic::{set_hook, take_hook},
+    path::PathBuf,
+    process::exit,
+};
+
+use serde_yaml::from_reader;
+use structopt::StructOpt;
+use tracing::{error, instrument};
+
+use stream_fitter::{
+    errors::FitterResult,
+    pipe_fitter::{PipeFitter, PipeFitterConfig},
+};
+
+#[derive(StructOpt)]
+struct StreamFitterCli {
+    #[structopt(parse(from_os_str))]
+    config_file: PathBuf,
+}
+
+fn entrypoint() -> FitterResult<()> {
+    pretty_env_logger::try_init()?;
+
+    let cli = StreamFitterCli::from_args();
+
+    let fitter_config: PipeFitterConfig = from_reader(File::open(cli.config_file)?)?;
+
+    let mut fitter = PipeFitter::from_config(fitter_config)?;
+
+    fitter.run()
+}
+
+#[instrument]
 fn main() {
-    println!("Hello, world!");
+    let panic_default = take_hook();
+    set_hook(Box::new(move |info| {
+        panic_default(info);
+        exit(1);
+    }));
+
+    exit(match entrypoint() {
+        Ok(_) => 0,
+        Err(err) => {
+            for e in err.iter_chain() {
+                error!("{}", e);
+            }
+            1
+        }
+    })
 }
